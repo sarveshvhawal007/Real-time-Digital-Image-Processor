468e83209c24d437cf9628cf089869a704f75540
sarveshvhawal007 <56724225+sarveshvhawal007@users.noreply.github.com>
2020-08-14 14:45:48 +0530

    Add files via upload

---
+++ parameter.v
@@ -0,0 +1,12 @@
+/*************************** **********************************************/
+/*************************** Definition file ******************************/
+/*************************** **********************************************/
+// By FPGA4student.com
+`define INPUTFILENAME		 "/home/sarvesh/Downloads/Verilog_Image_Processing/1.hex" // Input file name
+`define OUTPUTFILENAME		 "/home/sarvesh/Downloads/Verilog_Image_Processing/output.bmp"		// Output file name
+
+// Choose the operation of code by delete // in the beginning of the selected line
+
+//`define BRIGHTNESS_OPERATION
+//`define INVERT_OPERATION
+`define THRESHOLD_OPERATION 
